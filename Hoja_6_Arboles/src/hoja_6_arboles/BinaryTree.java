a75e7a12855b838f8a79628231c5f376b2a8f350
DaronP <per16362@uvg.edu.gt>
2018-03-17 17:58:31 -0500

    Mitad del BinaryTree

--- Hoja_6_Arboles/src/hoja_6_arboles/BinaryTree.java
+++ Hoja_6_Arboles/src/hoja_6_arboles/BinaryTree.java
@@ -12,4 +12,35 @@ package hoja_6_arboles;
  */
 public class BinaryTree {
     
+    Node root;
+    
+    public void addNode(String param){
+        Node newNode = Node(param);
+        
+        if(root == null){
+            root = newNode;
+        }
+        else{
+            Node focusNode = root;
+            Node parent;
+            
+            while(true){
+                parent = focusNode;
+                
+                if(param){
+                    focusNode = focusNode.leftChild;
+                }
+            }
+        }
+        
+    }
+    
+    public void inOrder(Node nodo){
+        if(nodo != null){
+            inOrder(nodo.leftChild);
+            System.out.println(nodo);
+            inOrder(nodo.rightChild);
+        }
+    }
+    
 }
