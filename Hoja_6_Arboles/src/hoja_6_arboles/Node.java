b6a228785b8b804ad9d65b5e8052477d5fabe747
Antonio <reytono@gmail.com>
2018-03-17 11:56:47 -0600

    Creación de clase BinaryTree y Node

---
+++ Hoja_6_Arboles/src/hoja_6_arboles/Node.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja_6_arboles;
+
+/**
+ * 
+ * @author Andres
+ * @author Antonio
+ */
+public class Node {
+    
+}
